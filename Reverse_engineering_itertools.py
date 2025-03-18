6f4edba2029dd541b26a5ef2167f45b94307f842
Ali Elb <68299151+alielbaha@users.noreply.github.com>
2025-03-18 15:38:14 +0100

    Add files via upload

---
+++ Reverse_engineering_itertools.py
@@ -0,0 +1,88 @@
+"""The vars function generates phonetically similar 
+variations of a given word based on predefined phoneme replacements. 
+It processes the input word, matches phoneme sequences 
+from a dictionary (replacements), and constructs all possible phonetic variations"""
+
+# OUTDATED !!
+replacements = {
+    "a": ["A"],
+    "b": ["B"],
+    "d": ["D"],
+    "e": ["E", "É", "È", "Ê", "AI"],
+    "f": ["F", "PH"],
+    "i": ["I", "Y"],
+    "j": ["Y"],
+    "k": ["K", "C", "QU"],
+    "l": ["L", "LL"],
+    "m": ["M","MM"],
+    "n": ["N", "NN", "NE"],
+    "o": ["O", "AU", "EAU"],
+    "p": ["P"],
+    "r": ["R", "RR"],
+    "s": ["S", "SS"],
+    "t": ["T", "TT"],
+    "u": ["OU"],
+    "v": ["V"],
+    "w": ["W"],
+    "y": ["U"],
+    "z": ["Z", "S"],
+    "ø": ["EU", "ŒU"],
+    "ŋ": ["NG"],
+    "œ": ["Œ", "EU", "ŒU"],
+    "œ̃": ["UN", "UM"],
+    "ɑ": ["A"],
+    "ɑ̃": ["AN", "AM", "EN", "EM"],
+    "ɔ": ["O"],
+    "ɔ̃": ["ON", "OM"],
+    "ə": ["E"],
+    "ɛ": ["E", "É", "È", "Ê", "AI"],
+    "ɛː": ["E", "É", "È", "Ê", "AI"],
+    "ɛ̃": ["IN", "IM", "AIN", "EIN"],
+    "ɡ": ["G", "GU"],
+    "ɥ": ["U", "HU"],
+    "ɲ": ["GN"],
+    "ʁ": ["R"],
+
+    "wa": ["OIT","OI","OUA","WA","UA"],
+
+
+    "ʃ": ["CH","SH"],
+    "ʒ": ["J", "G", "GE"],
+}
+
+
+import itertools
+
+def vars(name):
+    """
+    Param:
+    name (str): The input word in phonetic transcription accroding to IPA convention.
+
+    Returns:
+    list: A list of possible phonetic variations.
+    """
+    lenname = len(name)
+    incr = 0
+    variations = []
+
+    for ind in range(lenname):
+        for length in range(0, lenname - incr + 1):
+            substring = name[incr:lenname - length]
+            if substring in replacements:
+                incr += len(substring)
+                
+                if variations == []:
+                    variations = replacements[substring]
+                else:
+                    variations = list(itertools.product(variations, replacements[substring]))
+                    variations = [''.join(x) for x in variations]
+                break #leave the loop when finding the longest variations
+    return variations
+
+"""
+Remarks :
+ - it iterates through the input word character by character.
+
+ - matches the LONGEST PHONEME first (which wasn't the case with graphviz).
+
+ - generates phonetic variations using itertools.product to create combinations of phoneme replacements."""
